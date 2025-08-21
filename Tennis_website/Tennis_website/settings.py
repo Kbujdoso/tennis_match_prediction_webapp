06f8191db43f33efaf80857f338f34e9d5ac7c70
Kbujdoso <bkoppany6050@gmail.com>
2025-08-21 12:51:24 +0200

    allowed host

--- Tennis_website/Tennis_website/settings.py
+++ Tennis_website/Tennis_website/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-zy-7ixw5)jk+aqyc)@+!t=+nan$pr-xey+@)tgd3f!f=oe-qvg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tennis-match-prediction-webapp.onrender.com']
+
 
 
 # Application definition
