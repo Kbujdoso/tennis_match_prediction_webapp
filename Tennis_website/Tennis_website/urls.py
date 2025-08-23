41e7f4ea1e881c20d6a7d0610e50dbf37f236fca
Kbujdoso <bkoppany6050@gmail.com>
2025-08-23 12:14:46 +0200

    docker21

--- Tennis_website/Tennis_website/urls.py
+++ Tennis_website/Tennis_website/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('prediction.urls'))
 ]
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
