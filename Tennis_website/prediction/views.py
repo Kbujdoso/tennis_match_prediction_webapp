7240a4993d94b0f8d6f74c4f120f33c27a6e9b64
Kbujdoso <bkoppany6050@gmail.com>
2025-08-23 11:15:40 +0200

    docker15

--- Tennis_website/prediction/views.py
+++ Tennis_website/prediction/views.py
@@ -11,7 +11,10 @@ import json
 from django.http import JsonResponse
 
 from rapidfuzz import process
+import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+csv_path = os.path.join(BASE_DIR, "..","Tennis_match_prediction", "player_features.csv")
 
 player_list = []
 with open('/home/koppany/Tennis_website/Tennis_match_prediction/player_features.csv', newline='', encoding='utf-8') as csvfile:
