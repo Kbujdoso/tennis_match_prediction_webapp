32db6cbac07c745e9879558417a2ba92a6070ee9
Kbujdoso <bkoppany6050@gmail.com>
2025-08-24 12:06:41 +0200

    docker30

--- Tennis_website/Tennis_website/urls.py
+++ Tennis_website/Tennis_website/urls.py
@@ -23,7 +23,3 @@ urlpatterns = [
     path('', include('prediction.urls'))
 ]
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
